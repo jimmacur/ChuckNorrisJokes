aef1c557ecc3bdb80ede812904c8438ea006995b
Jim Macur <jim.macur@gmail.com>
2024-11-11 11:41:50 -0700

    feat: adds main Chuck page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [joke, setJoke] = useState('');
+  const [showReaction, setShowReaction] = useState(false);
+  const [message, setMessage] = useState('');
+
+  const getJoke = async () => {
+    try {
+      const response = await fetch ('https://api.chucknorris.io/jokes/random');
+      const data = await response.json();
+      setJoke(data.value);
+      setShowReaction(true);
+      setMessage('');
+    } catch (error) {
+      console.error("Error fetching joke:", error);
+    }
+  };
+
+  const relaxChuck = () => {
+    if (joke === '') {
+      setMessage('Chuck Norris is always relaxed.');
+    } else {
+      setShowReaction(false);
+      setJoke('');
+      setMessage(`Chuck Norris was always relaxed, even if it didn't look like it to you.`);
+    }
+    
+    setShowReaction(false);
+    setJoke('');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <h1 className='main-header'>Chuck Norris Jokes</h1>
+      <img 
+        src={showReaction ? '/assets/unfriendly_chuck.jpg' : '/assets/friendly_chuck.jpg'}
+        className='friendly-chuck' 
+        alt='Chuck Norris' 
+      />
+      <button onClick={getJoke} className='joke-button'>Get a Joke</button>
+      {joke && <p className='joke'>{joke}</p>}
+      {showReaction && <p className='reaction'>Chuck Norris is not amused.</p>}
+
+      <button onClick={relaxChuck} className='relax-button'>Relax Chuck</button>
+      {message && <p className='message'>{message}</p>}
+    </main>
+  )
 }
 
 export default App;
